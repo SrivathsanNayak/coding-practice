f29595378adf5c6f3457093964de49e5aeeea3a0
Srivathsan Nayak <58327763+SrivathsanNayak@users.noreply.github.com>
2022-06-02 16:12:48 +0530

    Time: 36 ms (9.73%), Space: 13.4 MB (79.49%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.py
+++ 709-to-lower-case/709-to-lower-case.py
@@ -4,4 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        return s.lower()
+        lower = ""
+        for i in s:
+            if ord(i) >= 65 and ord(i) <= 90:
+                lower += chr(ord(i) + 32)
+            else:
+                lower += i
+        return lower
+                
