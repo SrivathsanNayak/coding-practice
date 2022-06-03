16d39a72481c53111f7f0866a401f46d1a15005e
Srivathsan Nayak <58327763+SrivathsanNayak@users.noreply.github.com>
2022-06-03 08:21:46 +0530

    Time: 49 ms (70.27%), Space: 14.7 MB (39.42%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.py
+++ 242-valid-anagram/242-valid-anagram.py
@@ -5,6 +5,5 @@ class Solution(object):
         :type t: str
         :rtype: bool
         """
-        if sorted(s) == sorted(t):
-            return True
-        return False
+        return sorted(s) == sorted(t)
+        
