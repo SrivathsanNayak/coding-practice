ad6f633c65c44b44df2c801b6f9559584b711a80
Srivathsan Nayak <58327763+SrivathsanNayak@users.noreply.github.com>
2022-06-14 09:24:36 +0530

    Time: 839 ms (90.41%), Space: 22.7 MB (53.84%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,12 @@
 class Solution(object):
     def maxProfit(self, prices):
-        #refer to top solution
-        leftPtr = 0
-        rightPtr = 1
+        minPrice = float('inf')
         maxProfit = 0
-        while rightPtr < len(prices):
-            currentProfit = prices[rightPtr] - prices[leftPtr]
-            if currentProfit > 0:
-                maxProfit = max(currentProfit, maxProfit)
-            else:
-                leftPtr = rightPtr
-            rightPtr += 1
+        
+        for price in prices:
+            minPrice = min(minPrice, price)
+            curProfit = price - minPrice
+            maxProfit = max(curProfit, maxProfit)
+            
         return maxProfit
+            
