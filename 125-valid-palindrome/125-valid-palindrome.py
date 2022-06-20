bee30ceaab4708556a0dc5e47b62b2df7fa28969
Srivathsan Nayak <58327763+SrivathsanNayak@users.noreply.github.com>
2022-06-20 10:15:04 +0530

    Time: 49 ms (69.57%), Space: 15.3 MB (25.39%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -5,8 +5,5 @@ class Solution(object):
         :rtype: bool
         """
         string = ''.join(c.lower() for c in s if c.isalnum())
-        if string == string[::-1]:
-            return True
-        else:
-            return False
+        return string == string[::-1]
         
