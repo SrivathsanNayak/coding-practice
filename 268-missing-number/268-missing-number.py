d8e28658ca23899725f26d56eccae40ad8433f19
Srivathsan Nayak <58327763+SrivathsanNayak@users.noreply.github.com>
2022-07-02 12:11:29 +0530

    Time: 216 ms (28.77%), Space: 14.7 MB (42.79%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -4,13 +4,5 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        r = len(nums)
-        if nums == range(r + 1):
-            return r + 1
-        s = 0
-        for i in nums:
-            s ^= i
-        for i in range(r+1):
-            s ^= i
-        return s
+        return sum(range(len(nums) + 1)) - sum(nums)
             
