57f209ca46bdac40ab3f4c3b5d497d61e6e2eefd
Srivathsan Nayak <58327763+SrivathsanNayak@users.noreply.github.com>
2022-07-12 11:27:53 +0530

    Time: 13 ms (94.34%), Space: 13.4 MB (61.77%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        #fibonacci series
+        a,b = 1,1
+        
+        for i in range(n - 1):
+            c = a + b
+            a = b
+            b = c
+        
+        return b
+            
