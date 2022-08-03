671da04605c06f83c4465936a09e8db2420be9ee
Srivathsan Nayak <58327763+SrivathsanNayak@users.noreply.github.com>
2022-08-03 21:43:28 +0530

    Time: 1041 ms (24.12%), Space: 14.4 MB (41.23%) - LeetHub

---
+++ 729-my-calendar-i/729-my-calendar-i.py
@@ -0,0 +1,23 @@
+class MyCalendar(object):
+
+    def __init__(self):
+        self.calendar = []
+
+    def book(self, start, end):
+        """
+        :type start: int
+        :type end: int
+        :rtype: bool
+        """
+        for s,e in self.calendar:
+            #clash
+            if s < end and start < e:
+                return False
+        self.calendar.append((start, end))
+        return True
+        
+
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
